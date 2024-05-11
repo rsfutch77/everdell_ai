b32db1f7f8f1ae6e722495a198dbbcabe13fd5c1
Robert S Futch <robertsfutch@gmail.com>
2024-05-11 03:14:15 -0700

    Fix smaller rewards for mid game and no card reward if losing on current turn

--- player.py
+++ player.py
@@ -21,19 +21,21 @@ class AIPlayer(ReinforcementLearningAgent):
     def get_reward(self, game, action, done, agent_index, ties):
         # Reward function that gives a small reward for playing a high point value card,
         # a penalty for not playing a card, and a larger reward for winning the game.
+        # and a larger reward for winning the game.
         agents = game.agents  # Access the agents from the game object
-        if action is not None:
-            reward = action.points * 0.1  # Small reward for the card's point value
-        else:
-            reward = -0.1  # Penalty for not playing a card
+        reward = 0  
+        
         if done:
-
             tie_calculator, winner, winner_index = game.get_winner(game)
-            
             if tie_calculator > 1 and agents[agent_index].score == agents[winner_index].score:
                 reward += 0.5  # Smaller reward for a tie
             elif agent_index == winner:
                 reward += 1.0  # Large reward for winning
-            # No additional reward or penalty if the AI loses
+            else:
+                reward = 0 # No additional reward or penalty if the AI loses
+        elif action is not None:
+            reward = action.points * 0.01  # Small reward for the card's point value
+        else:
+            reward = 0  # No penalty for not playing a card
             
         return reward
